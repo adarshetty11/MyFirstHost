a36deeb9ae5ec141720cfaa9bcdf93d28c9f8e12
Adarsh Shetty <61731616+adarshetty11@users.noreply.github.com>
2020-03-15 17:12:11 +0530

    Update loginpage.php

--- loginpage.php
+++ loginpage.php
@@ -42,12 +42,13 @@
 
 <?php
 
-    $servername = "ec2-3-91-112-166.compute-1.amazonaws.com";
-    $username = "wncysqgsdbushb";
-    $password = "b8188545ffab5ba8643606c50da6cb46c5a5db1ab48dea7dd187e6f66158b70d";
-    $databasename = "dfao2a1rbfvq49";
+   // $servername = "ec2-3-91-112-166.compute-1.amazonaws.com";
+   // $username = "wncysqgsdbushb";
+   // $password = "b8188545ffab5ba8643606c50da6cb46c5a5db1ab48dea7dd187e6f66158b70d";
+   // $databasename = "dfao2a1rbfvq49";
 
-    $connection = pg_connect( $servername,$username,$password,$databasename);
+    $connection = pg_connect("host=ec2-3-91-112-166.compute-1.amazonaws.com port=5432 dbname=dfao2a1rbfvq49 user=wncysqgsdbushb password=b8188545ffab5ba8643606c50da6cb46c5a5db1ab48dea7dd187e6f66158b70d"
+);
 
     if(!$connection)
     {
