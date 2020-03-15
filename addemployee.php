f2340860a084ac21bc3ab8de6091da88913e2a31
Adarsh Shetty <61731616+adarshetty11@users.noreply.github.com>
2020-03-15 17:51:56 +0530

    Update addemployee.php

--- addemployee.php
+++ addemployee.php
@@ -102,7 +102,7 @@
     $salary = filter_input(INPUT_POST,'salary');
 
 
-        $connection = mysqli_connect( "host=ec2-3-91-112-166.compute-1.amazonaws.com port=5432 dbname=dfao2a1rbfvq49 user=wncysqgsdbushb password=b8188545ffab5ba8643606c50da6cb46c5a5db1ab48dea7dd187e6f66158b70d"
+        $connection = pg_connect( "host=ec2-3-91-112-166.compute-1.amazonaws.com port=5432 dbname=dfao2a1rbfvq49 user=wncysqgsdbushb password=b8188545ffab5ba8643606c50da6cb46c5a5db1ab48dea7dd187e6f66158b70d"
 );
 
             if(!$connection)
