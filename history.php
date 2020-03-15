8bf97644195217439dff4072e857cf102aa9da02
Adarsh Shetty <61731616+adarshetty11@users.noreply.github.com>
2020-03-15 19:46:45 +0530

    Update history.php

--- history.php
+++ history.php
@@ -1,32 +1,28 @@
 <?php
 
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $databasename = "myfirstdb";
-
-    $connection = mysqli_connect( $servername,$username,$password,$databasename);
+    $connection = pg_connect( "host=ec2-3-91-112-166.compute-1.amazonaws.com port=5432 dbname=dfao2a1rbfvq49 user=wncysqgsdbushb password=b8188545ffab5ba8643606c50da6cb46c5a5db1ab48dea7dd187e6f66158b70d"
+);
 
     if(!$connection)
     {
-        die("Connection failed: ".mysqli_connect_error());
+        die("Connection failed: ".pg_last_error());
     }
     
     session_start();
     $username = $_SESSION['user'];
 
-    $query1 = "Select Name from Customer where Username='$username';";
-    $result = $connection -> query($query1);
-    $name1 = $result->fetch_assoc();
+    $query1 = "Select Name from customer where username='$username';";
+    $result = pg_query($connection,$query1);
+    $name1 = pg_fetch_assoc($result);
 
-    $query = "SELECT d.DepartmentName, k.Kit, k.Brand, k.Size, k.Price from Department d,Kits k 
-    where k.departmentNo=d.departmentNo and k.Username='$username';";
+    $query = "SELECT d.departmentname, k.kit, k.brand, k.size, k.price from department d,kits k 
+    where k.departmentno=d.departmentno and k.username='$username';";
 
-    $result = mysqli_query($connection,$query);
+    $result = pg_query($connection,$query);
 
-    $query1 = "select Name from Customer where Username = '$username';";
-    $result1 = mysqli_query($connection,$query1);
-    $data = mysqli_fetch_assoc($result1);
+    $query1 = "select name from customer where username = '$username';";
+    $result1 = pg_query($connection,$query1);
+    $data = pg_fetch_assoc($result1);
     $name = $data['Name'];
 ?>
 
@@ -47,7 +43,7 @@
         <nav>
         <img src="css/images/icon1.png">
                     <ul style="margin-top:-34px;margin-left:22px;">   
-                            <li style="text-transform:uppercase;"><?php echo $name1['Name']; ?></li>
+                            <li style="text-transform:uppercase;"><?php echo $name1['name']; ?></li>
                             <li><a style="color:yellow" href="history.php">Purchase History </a></li>
                             <li><a href="customerlogin.php">Sign Out</a></li>
                     </ul>
@@ -70,16 +66,16 @@
             </tr>
 
     <?php
-    while($rows = $result -> fetch_assoc())
+    while($rows = pg_fetch_assoc($result))
     {
     ?>
 
     <tr align="center">
-        <td><?php echo $rows['DepartmentName'] ?></td>
-        <td><?php echo $rows['Kit'] ?></td>
-        <td><?php echo $rows['Brand'] ?></td>
-        <td><?php echo $rows['Size'] ?></td>
-        <td><?php echo $rows['Price'] ?></td>
+        <td><?php echo $rows['departmentname'] ?></td>
+        <td><?php echo $rows['kit'] ?></td>
+        <td><?php echo $rows['brand'] ?></td>
+        <td><?php echo $rows['size'] ?></td>
+        <td><?php echo $rows['price'] ?></td>
     </td>
     </tr>
 
