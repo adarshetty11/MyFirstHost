14e3bacb4aef50046258ffd65ba53ed4db20f8aa
Adarsh Shetty <61731616+adarshetty11@users.noreply.github.com>
2020-03-15 17:50:17 +0530

    Update addemployee.php

--- addemployee.php
+++ addemployee.php
@@ -102,21 +102,17 @@
     $salary = filter_input(INPUT_POST,'salary');
 
 
-            $servername = "localhost";
-            $username = "root";
-            $password = "";
-            $databasename = "myfirstdb";
-
-            $connection = mysqli_connect( $servername,$username,$password,$databasename);
+        $connection = mysqli_connect( "host=ec2-3-91-112-166.compute-1.amazonaws.com port=5432 dbname=dfao2a1rbfvq49 user=wncysqgsdbushb password=b8188545ffab5ba8643606c50da6cb46c5a5db1ab48dea7dd187e6f66158b70d"
+);
 
             if(!$connection)
             {
-                die("Connection failed: ".mysqli_connect_error());
+                die("Connection failed: ".pg_last_error());
             }
 
             if(isset($salary))
             {
-                $insertRecordQuery = "INSERT into Employee (EmployeeId, Name, DepartmentNo ,Gender, DOB, Address, Salary) values 
+                $insertRecordQuery = "INSERT into employee (employeeid, name, departmentno ,gender, dob, address, salary) values 
                 ('$empid', '$name', '$department','$gender', '$dob', '$address', '$salary');";
                 $result = mysqli_query($connection,$insertRecordQuery);
                 if($result)
@@ -129,7 +125,7 @@
                 }
             }
 
-            $closeConnection = mysqli_close($connection);
+            $closeConnection = pg_close($connection);
 
 ?>
 
