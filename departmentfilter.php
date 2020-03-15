d994ce3bc8be75c2884f19d9e2654aa552bf443b
Adarsh Shetty <61731616+adarshetty11@users.noreply.github.com>
2020-03-15 19:11:38 +0530

    Update departmentfilter.php

--- departmentfilter.php
+++ departmentfilter.php
@@ -1,19 +1,16 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $databasename = "myfirstdb";
-
-    $connection = mysqli_connect( $servername,$username,$password,$databasename);
+    
+    $connection = pg_connect( "host=ec2-3-91-112-166.compute-1.amazonaws.com port=5432 dbname=dfao2a1rbfvq49 user=wncysqgsdbushb password=b8188545ffab5ba8643606c50da6cb46c5a5db1ab48dea7dd187e6f66158b70d"
+);
 
     if(!$connection)
     {
-         die("Connection failed: ".mysqli_connect_error());
+         die("Connection failed: ".pg_last_error());
     }
 
     session_start();
-    $query = "select * from Department;";
-    $result = mysqli_query($connection,$query);
+    $query = "select * from department;";
+    $result = pg_query($connection,$query);
 ?>
 
 <!DOCTYPE html>
@@ -59,13 +56,13 @@
             </tr>
 
     <?php
-    while($rows = $result -> fetch_assoc())
+    while($rows = pg_fetch_assoc($result))
     {
     ?>
     <tr align="center">
-            <td><?php echo $rows['DepartmentNo'] ?></td>
-            <td><?php echo $rows['DepartmentName'] ?></td>
-            <td><a href="employeefilter.php?check=<?php echo $rows['DepartmentNo'] ?>">Check</a></td>  
+            <td><?php echo $rows['departmentno'] ?></td>
+            <td><?php echo $rows['departmentname'] ?></td>
+            <td><a href="employeefilter.php?check=<?php echo $rows['departmentno'] ?>">Check</a></td>  
     </tr>
 
     <?php
